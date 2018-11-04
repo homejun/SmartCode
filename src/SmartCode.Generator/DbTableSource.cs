300772bb7bc4b2728db52b82759d8ce9f7a76343
homejun <junhome@sina.com>
2018-11-04 15:47:50 +0800

    1、数据源改为sqlite 2、sqlite字段与C#类型处理修正：字符串类型、自增字段 3、取表时去掉sequence、索引

--- src/SmartCode.Generator/DbTableSource.cs
+++ src/SmartCode.Generator/DbTableSource.cs
@@ -32,6 +32,23 @@ namespace SmartCode.Generator
             {
                 foreach (var col in table.Columns)
                 {
+                    if ((DbRepository.DbProvider == Db.DbProvider.SQLite) && Project.Language == "CSharp")
+                    {
+                        if (col.DbType.IndexOf("(") > -1)
+                        {
+                            string[] arr = col.DbType.Split('(');
+                            col.DbType = arr[0];
+                        }
+                        else
+                            if (col.IsPrimaryKey == true)  //从 SQLite 的 2.3.4 版本开始，如果你将一个表中的一个字段声明为 INTEGER PRIMARY KEY，那么无论你何时向该表的该字段插入一个 NULL 值，这个 NULL 值将自动被更换为比表中该字段所有行的最大值大 1 的整数；如果表为空，那么将被更换为 1。
+                        {
+                            if (col.DbType == "integer")
+                            {
+                                col.AutoIncrement = true;
+                            }
+
+                        }
+                    }
                     if ((DbRepository.DbProvider == Db.DbProvider.MySql || DbRepository.DbProvider == Db.DbProvider.MariaDB)
                         && col.DbType == "char"
                         && col.DataLength == 36
